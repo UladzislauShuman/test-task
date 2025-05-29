d0918bc326bd913fa32dd27c72beddf59a965b06
UladzislauLinux <fpm.shumanVO@bsu.by>
2025-05-29 15:05:10 +0300

    made task but with changes in class Session and added DeviceTyoeConverter to convert int code into DeviceType and back again

--- src/main/java/com/example/java_ifortex_test_task/service/SessionService.java
+++ src/main/java/com/example/java_ifortex_test_task/service/SessionService.java
@@ -1,26 +1,41 @@
 package com.example.java_ifortex_test_task.service;
 
 import com.example.java_ifortex_test_task.dto.SessionResponseDTO;
+import com.example.java_ifortex_test_task.entity.DeviceType;
+import com.example.java_ifortex_test_task.entity.Session;
 import com.example.java_ifortex_test_task.mapper.SessionMapper;
 import com.example.java_ifortex_test_task.repository.SessionRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SessionService {
+    private static final String SESSION_NOT_FOUND = "session not found";
+    private static final LocalDateTime endDate = LocalDateTime.of(2025, 1, 1, 0, 0);
+
     private final SessionRepository sessionRepository;
     private final SessionMapper sessionMapper;
 
     // Returns the first (earliest) desktop Session
     public SessionResponseDTO getFirstDesktopSession() {
-        return null;
+        Session session = sessionRepository.getFirstDesktopSession(DeviceType.DESKTOP);
+        if (session == null) {
+            throw new EntityNotFoundException(SESSION_NOT_FOUND);
+        }
+        return sessionMapper.toDto(session);
     }
 
     // Returns only Sessions from Active users that were ended before 2025
     public List<SessionResponseDTO> getSessionsFromActiveUsersEndedBefore2025() {
-        return null;
+        List<Session> sessions = sessionRepository.getSessionsFromActiveUsersEndedBefore2025(endDate);
+        return sessions.stream()
+                .map(sessionMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/example/java_ifortex_test_task/service/UserService.java
+++ src/main/java/com/example/java_ifortex_test_task/service/UserService.java
@@ -1,28 +1,40 @@
 package com.example.java_ifortex_test_task.service;
 
 import com.example.java_ifortex_test_task.dto.UserResponseDTO;
+import com.example.java_ifortex_test_task.entity.DeviceType;
+import com.example.java_ifortex_test_task.entity.User;
 import com.example.java_ifortex_test_task.mapper.UserMapper;
 import com.example.java_ifortex_test_task.repository.SessionRepository;
 import com.example.java_ifortex_test_task.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    public static final String USER_WITH_MOST_OF_SESSIONS_NOT_FOUND_MESSAGE = "User with most of sessions not found";
     private final UserRepository userRepository;
     private final SessionRepository sessionRepository;
     private final UserMapper userMapper;
 
     // Returns a User with the biggest amount of sessions
     public UserResponseDTO getUserWithMostSessions() {
-        return null;
+        User user = userRepository.getUserWithMostSessions();
+        if (user == null) {
+            throw new EntityNotFoundException(USER_WITH_MOST_OF_SESSIONS_NOT_FOUND_MESSAGE);
+        }
+        return userMapper.toDto(user);
     }
 
     // Returns Users that have at least 1 Mobile session
     public List<UserResponseDTO> getUsersWithAtLeastOneMobileSession() {
-        return null;
+        List<User> users = userRepository.getUsersWithAtLeastOneMobileSession(DeviceType.MOBILE);
+        return users.stream()
+                .map(userMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
